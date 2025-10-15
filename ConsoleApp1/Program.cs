ea0d84a6905eb4e1f7a2387a46d950a587f2da06
234933 <234933@KB37-118-C11>
2025-10-15 14:34:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class Person
+{
+    private int _id;
+    private string _name;
+    private int _age;
+    private string _email;
+
+    protected Person(int id, string name, int age, string email)
+    {
+        _id = id;
+        _name = name;
+        _age = age;
+        _email = email;
+    }
+
+    public int Id => _id;
+    public string Name => _name;
+    public int Age => _age;
+    public string Email => _email;
+
+    public abstract string GetRole();
+
+    public virtual string GetInfo()
+    {
+        return $"ID: {_id}, Имя: {_name}, Возраст: {_age}";
+    }
+}
+
