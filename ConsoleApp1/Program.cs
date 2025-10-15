96be7e0cdff9cdf33bc2d7047a9151fa8dad495a
ruslanfedotov <fedotovruslan617@gmail.com>
2025-10-15 14:41:06 +0300

    класс с информацией о курсках

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -73,5 +73,95 @@ public class Student : Person
     }
 }
 
+public class Teacher : Person
+{
+    private string _department;
+    private List<Course> _teachingCourses;
+
+    public Teacher(int id, string name, int age, string email, string department)
+        : base(id, name, age, email)
+    {
+        _department = department;
+        _teachingCourses = new List<Course>();
+    }
+
+    public string Department => _department;
+    public List<Course> TeachingCourses => _teachingCourses;
+
+    public override string GetRole() => "Преподаватель";
+
+    public override string GetInfo()
+    {
+        return base.GetInfo() + $", Кафедра: {_department}";
+    }
+
+    public void AssignToCourse(Course course)
+    {
+        if (!_teachingCourses.Contains(course))
+        {
+            _teachingCourses.Add(course);
+            course.AssignTeacher(this);
+        }
+    }
+}
+
+public class Course
+{
+    private int _id;
+    private string _name;
+    private string _description;
+    private Teacher _teacher;
+    private List<Student> _students;
+
+    public Course(int id, string name, string description)
+    {
+        _id = id;
+        _name = name;
+        _description = description;
+        _students = new List<Student>();
+        _teacher = null;
+    }
+
+    public int Id => _id;
+    public string Name => _name;
+    public string Description => _description;
+    public Teacher Teacher => _teacher;
+    public List<Student> Students => _students;
+
+    public string GetInfo()
+    {
+        string teacherInfo = _teacher != null ? _teacher.Name : "Не назначен";
+        return $"Курс: {_name}\nОписание: {_description}\n" +
+               $"Преподаватель: {teacherInfo}\n" +
+               $"Студентов: {_students.Count}";
+    }
+
+    public void AssignTeacher(Teacher teacher)
+    {
+        _teacher = teacher;
+    }
+
+    public void AddStudent(Student student)
+    {
+        if (!_students.Contains(student))
+        {
+            _students.Add(student);
+        }
+    }
+
+    public string GetStudentList()
+    {
+        if (_students.Count == 0)
+            return "На курс не записаны студенты";
+
+        string studentList = "";
+        foreach (var student in _students)
+        {
+            studentList += $"- {student.Name} ({student.Specialty})\n";
+        }
+        return studentList;
+    }
+}
+
 
 
